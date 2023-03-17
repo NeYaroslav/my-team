481aa4a34d8ace7489e37c562ff63adfd45f827f
Yaroslav Nazarenko <yinazarenkoo@gmail.com>
2023-03-18 01:14:40 +0200

    rest-api-comit

--- server/src/controllers/register.ts
+++ server/src/controllers/register.ts
@@ -8,12 +8,12 @@ const register = async (req: Request, res: ResponseWithError) => {
   try {
     const hashedPassword = await bcrypt.hash(password, 10)
     await registerUser(username, hashedPassword)
+    res.redirect(307, './login')
   } catch (error) {
     return res
       .status(400)
       .json({ message: 'Could not register the user with this username' })
   }
-  res.redirect(307, './login')
 }
 
 export default register
